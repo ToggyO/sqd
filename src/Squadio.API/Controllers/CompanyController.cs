68831a5b5cb0794317cb5549978b315d748512fd
Valentin Karpov <karpov@magora-systems.com>
2019-10-11 11:24:55 +0700

    Added company controller.

---
+++ src/Squadio.API/Controllers/CompanyController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Squadio.API.Handlers.Auth;
+
+namespace Squadio.API.Controllers
+{
+    [ApiController]
+    [Route("api/company")]
+    public class CompanyController : ControllerBase
+    {
+        private readonly IAuthHandler _handler;
+
+        public CompanyController(IAuthHandler handler)
+        {
+            _handler = handler;
+        }
+        
+        [HttpGet]
+        public string GetCompany()
+        {
+            return "Company";
+        }
+    }
+}
