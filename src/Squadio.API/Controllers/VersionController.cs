5a8823034eabdd7d6e6654b8c6ebc6b82f6521ab
Valentin Karpov <karpov@magora-systems.com>
2019-11-22 12:06:07 +0700

    Updated version number to '0.1.7 b'

--- src/Squadio.API/Controllers/VersionController.cs
+++ src/Squadio.API/Controllers/VersionController.cs
@@ -8,7 +8,7 @@ namespace Squadio.API.Controllers
     [Route("api/versions")]
     public class VersionController : ControllerBase
     {
-        private const string Version = "0.1.6 b";
+        private const string Version = "0.1.7 b";
 
         public VersionController()
         {
