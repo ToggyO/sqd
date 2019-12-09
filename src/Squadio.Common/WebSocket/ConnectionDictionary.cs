c782feced36cf8e27318b3f9b05bc91c4985e53c
Valentin Karpov <karpov@magora-systems.com>
2019-12-09 12:24:10 +0700

    Added singleton connection dictionary.

---
+++ src/Squadio.Common/WebSocket/ConnectionDictionary.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Squadio.Common.WebSocket
+{
+    public class ConnectionDictionary<T>
+    {
+        private ConnectionDictionary(){}
+        private static readonly ConnectionDictionary<T> _instance = new ConnectionDictionary<T>();
+        public static ConnectionDictionary<T> GetInstance() => _instance;
+        
+        private readonly Dictionary<T, HashSet<string>> _connections =
+            new Dictionary<T, HashSet<string>>();
+        
+        public void Add(T key, string connectionId)
+        {
+            lock (_connections)
+            {
+                if (!_connections.TryGetValue(key,out var connId))
+                {
+                    connId = new HashSet<string>();
+                    _connections.Add(key, connId);
+                }
+                lock (connId)
+                {
+                    connId.Add(connectionId);
+                }
+            }
+        }
+        
+        public void Remove(T key, string connectionId)
+        {
+            lock (_connections)
+            {
+                if(!_connections.TryGetValue(key,out var connId))
+                { return; }
+                lock (connId)
+                {
+                    connId.Remove(connectionId);
+                    if (connId.Count == 0)
+                    { _connections.Remove(key); }
+                }
+            }
+        }
+        
+        public IEnumerable<string> GetUserConnections(T key)
+        {
+            lock (_connections)
+            {
+                if (_connections.TryGetValue(key,out var connId))
+                {
+                    return connId;
+                }
+            }
+
+            return Enumerable.Empty<string>();
+        }
+    }
+}
