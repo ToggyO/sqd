e22aa2560b59667c0cca5ec30ce2b2b680d30109
Valentin Karpov <karpov@magora-systems.com>
2019-12-09 12:36:31 +0700

    Changes for deploy.

--- src/Squadio.API/WebSocketHubs/ChatHub.cs
+++ src/Squadio.API/WebSocketHubs/ChatHub.cs
@@ -12,46 +12,26 @@ namespace Squadio.API.WebSocketHubs
     public class ChatHub : Hub
     {
         private readonly ILogger<ChatHub> _logger;
-        private readonly IUsersHandler _handler;
         
-        public ChatHub(ILogger<ChatHub> logger
-            , IUsersHandler handler)
+        public ChatHub(ILogger<ChatHub> logger)
         {
             _logger = logger;
-            _handler = handler;
             _logger.LogInformation("ChatHub constructor work done");
         }
-        
+
         [HubMethodName("SendMessage")]
         public async Task SendMessage(SimpleMessage model)
         {
             _logger.LogInformation($"Enter into ChatHub.SendMessage from '{model.Username}' with message: {model.Message}");
-            try
+            
+            var message = model.Message;
+            var newMessage = new SimpleMessage
             {
-                if(model.Message == "throw")
-                    throw new Exception();
-                var message = model.Message;
-                var userResponse = await _handler.GetCurrentUser(Context.User);
-                if (userResponse.IsSuccess)
-                {
-                    if (userResponse.IsSuccess)
-                    {
-                        message += " ||| " + JsonSerializer.Serialize(userResponse.Data);
-                    }
-                }
-                var newMessage = new SimpleMessage
-                {
-                    Message = message,
-                    Username = model.Username
-                };
-                
-                await Clients.All.SendAsync("ReceiveMessage" , newMessage);
-            }
-            catch (Exception e)
-            {
-                _logger.LogError($"ChatHub.SendMessage throw: {e.Message}");
-                await Clients.Caller.SendAsync("disconnect");
-            }
+                Message = message,
+                Username = model.Username
+            };
+
+            await Clients.All.SendAsync("ReceiveMessage", newMessage);
         }
     }
 
