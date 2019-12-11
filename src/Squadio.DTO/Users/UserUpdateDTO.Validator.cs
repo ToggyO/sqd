bbba1018954baa5051e73fc7353a3791ab4f3450
Valentin Karpov <karpov@magora-systems.com>
2019-12-11 13:44:15 +0700

    Added validation to update user.

---
+++ src/Squadio.DTO/Users/UserUpdateDTO.Validator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+using Squadio.Common.Models.Errors;
+
+namespace Squadio.DTO.Users
+{
+    public class UserUpdateDTOValidator: AbstractValidator<UserUpdateDTO>
+    {
+        public UserUpdateDTOValidator()
+        {
+            RuleFor(model => model.Name)
+                .NotEmpty()
+                .MaximumLength(60)
+                .WithErrorCode(ErrorCodes.Common.FieldInvalid);
+        }
+    }
+}
