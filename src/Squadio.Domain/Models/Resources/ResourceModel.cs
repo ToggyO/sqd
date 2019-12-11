6052138ec3c07af7207ac4beb2ac4bbce64d5b45
Valentin Karpov <karpov@magora-systems.com>
2019-12-11 14:19:18 +0700

    Add model.

---
+++ src/Squadio.Domain/Models/Resources/ResourceModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Squadio.Domain.Models.Resources
+{
+    public class ResourceModel
+    {
+        public Guid Id { get; set; }
+        public Guid UserId { get; set; }
+        public DateTime CreateDate { get; set; }
+        public string ContentType { get; set; }
+        public string FileName { get; set; }
+        public string Group { get; set; }
+    }
+}
