53f78280fbd07b9886c80a08d22dbafe5ad193cf
Valentin Karpov <karpov@magora-systems.com>
2019-12-20 14:36:42 +0700

    Updated version number to '0.6.0 b'

--- src/Squadio.API/Controllers/VersionController.cs
+++ src/Squadio.API/Controllers/VersionController.cs
@@ -17,7 +17,7 @@ namespace Squadio.API.Controllers
     [Route("api/versions")]
     public class VersionController : ControllerBase
     {
-        private const string Version = "0.5.4 b";
+        private const string Version = "0.6.0 b";
         private readonly ILogger<VersionController> _logger;
 
         public VersionController(ILogger<VersionController> logger)
