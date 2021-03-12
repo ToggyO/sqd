5dcad51bc78131febe155523ca57cc968816e611
Valentin Karpov <karpov@magora-systems.com>
2021-03-12 17:09:46 +0700

    Fixed urls

--- src/Squadio.API/Startup.cs
+++ src/Squadio.API/Startup.cs
@@ -196,7 +196,7 @@ namespace Squadio.API
                     options.RouteTemplate = "swagger/{documentName}/swagger.json";
                     options.PreSerializeFilters.Add((swaggerDoc, httpReq) => swaggerDoc.Servers = new System.Collections.Generic.List<OpenApiServer>
                     {
-                        new OpenApiServer { Url = $"https://squad.magora.team/api", Description = "Dev"},
+                        new OpenApiServer { Url = $"https://api.squad.magora.team", Description = "Dev"},
                         new OpenApiServer { Url = $"{httpReq.Scheme}://{httpReq.Host.Value}", Description = "Local"}
                     });
                 });
